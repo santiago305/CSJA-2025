15d3ed01b259aa259c088dd2a0a4ef1cddf1c2c7
santiago305 <minecratf633@gmail.com>
2025-05-29 19:11:29 -0500

    1

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: ' https://santiago305.github.io/CSJA-2025'
+})
