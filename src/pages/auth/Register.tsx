c6fceaf0196426e5a723c8955b9af83dd44bba5d
santiago305 <minecratf633@gmail.com>
2025-05-29 20:06:43 -0500

    refactor: simplify Login and Register components by removing unnecessary wrapper divs

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -4,10 +4,6 @@ const RegisterForm = lazy(() => import("../../components/auth/RegisterForm"));
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <RegisterForm />
-      </div>
-    </div>
+    <RegisterForm />
   )
 }
