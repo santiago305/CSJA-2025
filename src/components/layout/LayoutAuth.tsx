db9f8d86065300b3afb52a5ac1c0f30e82c531e6
santiago305 <minecratf633@gmail.com>
2025-05-29 20:16:25 -0500

    Implement code changes to enhance functionality and improve performance

--- src/components/layout/LayoutAuth.tsx
+++ src/components/layout/LayoutAuth.tsx
@@ -1,8 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { RoutesPaths } from "../../router/config/routesPaths";
 
 export default function LayoutAuth() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div 
+    className="flex min-h-svh w-full items-center justify-center p-6 md:p-10"
+    style={{ 
+      backgroundImage: `url('${RoutesPaths.root}/assets/auth/bg-auth.jpg')`, 
+      backgroundSize: 'cover', 
+      backgroundPosition: 'center'
+     }}
+    >
       <div className="w-full max-w-sm">
           <Outlet/>
       </div>
