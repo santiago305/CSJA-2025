317ede04453f8aabfbf67949961a3274ed8cc4cc
santiago305 <minecratf633@gmail.com>
2025-05-31 00:51:27 -0500

    fix: adjust padding and alignment in TopSidebar component

--- src/components/topSidebar.tsx
+++ src/components/topSidebar.tsx
@@ -4,10 +4,10 @@ import Notification from "./Notification";
 export default function TopSidebar () {
   return (
     <div
-    className="w-full px-7 py-3 lg:py-5 bg-white shadow-md"
+    className="w-full px-7 py-1 lg:py-3 bg-white shadow-md"
     >
       <div
-      className="w-full h-auto flex justify-center items-center gap-5"
+      className="w-full h-auto flex justify-end items-center gap-5"
       >
         <Notification />
         <UserMenu />
