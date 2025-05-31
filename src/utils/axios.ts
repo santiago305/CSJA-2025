1a28187f8c37623adece2241108ec8ae5b70958b
santiago305 <minecratf633@gmail.com>
2025-05-31 12:42:16 -0500

    feat: restore axios response interceptor logic and error handling

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,62 +1,63 @@
-// import { logoutUser, refresh_token } from '@/services/authService';
+
 import axios from 'axios';
 import { envs } from '../config/envs';
+import { logoutUser, refresh_token } from '../service/authService';
 
 const axiosInstance = axios.create({
   baseURL: envs.apiUrl,
   withCredentials: true,
 });
 
-// let isRefreshing = false;
-// let failedQueue: Array<{
-//   resolve: (value?: any) => void,
-//   reject: (error: any) => void
-// }> = [];
+let isRefreshing = false;
+let failedQueue: Array<{
+  resolve: (value?: any) => void,
+  reject: (error: any) => void
+}> = [];
 
-// const processQueue = (error: any, token: string | null = null) => {
-//   failedQueue.forEach(prom => {
-//     if (error) {
-//       prom.reject(error);
-//     } else {
-//       prom.resolve(token);
-//     }
-//   });
-//   failedQueue = [];
-// };
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
 
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     const originalRequest = error.config;
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
 
-//     const isAuthEndpoint = originalRequest.url.includes('/auth/refresh');
-//     if (error.response?.status === 401 && !originalRequest._retry && !isAuthEndpoint) {
-//       if (isRefreshing) {
-//         return new Promise((resolve, reject) => {
-//           failedQueue.push({ resolve, reject });
-//         }).then(() => axiosInstance(originalRequest));
-//       }
+    const isAuthEndpoint = originalRequest.url.includes('/auth/refresh');
+    if (error.response?.status === 401 && !originalRequest._retry && !isAuthEndpoint) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        }).then(() => axiosInstance(originalRequest));
+      }
 
-//       originalRequest._retry = true;
-//       isRefreshing = true;
+      originalRequest._retry = true;
+      isRefreshing = true;
 
-//       try {
-//         const newToken = await refresh_token(); 
+      try {
+        const newToken = await refresh_token(); 
 
-//         processQueue(null, newToken);
+        processQueue(null, newToken);
 
-//         return axiosInstance(originalRequest);
-//       } catch (refreshError) {
-//         processQueue(refreshError, null);
-//         logoutUser();
-//         return Promise.reject(refreshError);
-//       } finally {
-//         isRefreshing = false;
-//       }
-//     }
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        processQueue(refreshError, null);
+        logoutUser();
+        return Promise.reject(refreshError);
+      } finally {
+        isRefreshing = false;
+      }
+    }
 
-//     return Promise.reject(error);
-//   }
-// );
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
