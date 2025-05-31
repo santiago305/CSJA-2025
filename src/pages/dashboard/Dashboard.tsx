669288e2eaab5dfe8aa10afc3a44a54c258ff110
santiago305 <minecratf633@gmail.com>
2025-05-31 18:42:27 -0500

    feat: restructure Dashboard component layout and enhance chart display sections

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,34 @@
 export default function Dashboard () {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to the dashboard! Here you can manage your account and view your data.</p>
-      {/* Aquí puedes agregar más componentes o funcionalidades del dashboard */}
+    <div
+    className="flex-1 h-full flex flex-col"
+    >
+      <h3
+      className="text-2xl font-semibold text-gray-800 dark:text-white mb-6"
+      >
+        Dashboard
+      </h3>
+
+      <div
+      className="w-full h-full flex flex-wrap gap-4"
+      >
+        <div className="min-w-[500px] flex-1 md:w-2/1 lg:w-3/1 p-4 bg-white rounded-3xl shadow-md">
+          <h4 className="text-xl font-semibold mb-4">Basic Area Chart</h4>
+        </div>
+          <div className="min-w-[500px] flex-1 md:w-2/1 lg:w-3/1 p-4 bg-white rounded-3xl shadow-md">
+          <h4 className="text-xl font-semibold mb-4">Area Spaline Chart</h4>
+        </div>  
+          <div className="min-w-[500px] flex-1 md:w-2/1 lg:w-3/1 p-4 bg-white rounded-3xl shadow-md">
+          <h4 className="text-xl font-semibold mb-4">Bar chat</h4>
+        </div>
+          <div className="min-w-[500px] flex-1 md:w-2/1 lg:w-3/1 p-4 bg-white rounded-3xl shadow-md">
+          <h4 className="text-xl font-semibold mb-4">Pie Chart</h4>
+        </div>
+          <div className="min-w-[500px] flex-1 md:w-2/1 lg:w-3/1 p-4 bg-white rounded-3xl shadow-md">
+          <h4 className="text-xl font-semibold mb-4">Mixed Char</h4>
+        </div>
+        
+      </div>
     </div>
   );
 }
